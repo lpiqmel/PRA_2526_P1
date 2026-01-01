235b49242dccd0659f4f6113c6515b92d5ff6451
LPIQMEL <lpiqmel@alumno.upv.es@ldsic-vdi07.upvnet.upv.es>
2026-01-01 19:44:55 +0100

    Añadida implementación de la clase Node

---
+++ Node.h
@@ -0,0 +1,21 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <ostream>
+
+template <typename T> 
+class Node {
+	public:
+    		T data;
+		Node<T>* next;
+
+		Node(T data, Node<T>* next=nullptr){}
+
+		friend std::ostream& operator<<(std::ostream &out, const Node<T> &node){
+			out << node.data;
+			return out;
+		}
+    
+};
+
+#endif
